d01565751d1452a0ae243f8d05323dae1484988c
Arai Lanju <pearcearai@gmail.com>
2023-03-14 11:04:01 +0800

    clean-up code update google stun to openrelay turn server

--- pages/app.py
+++ pages/app.py
@@ -1,3 +1,5 @@
+# https://blog.streamlit.io/common-app-problems-resource-limits/
+
 import streamlit as st
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
 from pathlib import Path
@@ -11,25 +13,23 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img, array_to_img
 
 
-# from streamlit_webrtc import webrtc_streamer
-
-# webrtc_streamer(key="sample")
-
 HERE = Path(__file__).parent
 # ROOT = HERE.parent
 
 
-## 'Path_to/my_model.h5'
 
-@st.cache_resource
+# NOTE: Cache to prevent resource overload
+@st.cache_resource    
 def load_model():
-    ## load model local
+    # --load model local--
     # tf.keras.models.load_model('saved_model/prettyfish_model')
-    ## load model streamlit
+    # --load model streamlit--
     # 
     return tf.keras.models.load_model(HERE / './saved_model/prettyfish_model')
  
 model = load_model()
+
+
 
 
 # class names
@@ -41,46 +41,55 @@ class_names = ['clownfish',
                'snakeskin_discus',
                'cuddlefish'] # Mars
 
-# inputs
+
+
+# -------------- Basic -----------------
+# def process_img (img):
+#     ## resize
+#     img = cv2.resize(img, (299,299))
+#     ## expand dims to 4D
+#     test_img = np.expand_dims(img, axis=0)
+#     ## log-outs of probabilities
+#     logits = model.predict(test_img)
+#     ## select index of highest logits
+#     predict_output = tf.argmax(logits, -1).numpy()
+#     ## run thru classifier to get name  ##
+#     pred_text = class_names[predict_output[0]]   
+#     return pred_text, logits  
+
+
+##---------------- Mars ------------------##
+# NOTE: use tf.nn.softmax to convert logits to probabilities
+# scaler: 0 Dimension tensor i.e ()
+# vector: 1 Dimension tensor i.e (9, )
+# matrix: 2 Dimension tensor i.e (1, 6)
+# tf.nn.softmax(): converts logits to probabilities
+# - output of tf.nn.softmax() is tf.EagerTensor 
+# - numpy(): converts tf.EagerTensor into np.array
+# - np.any(): any of the ele is true, return true 
 def process_img (img):
-    ## resiZe
+    ## resize
     img = cv2.resize(img, (299,299))
     ## expand dims to 4D
     test_img = np.expand_dims(img, axis=0)
     ## log-outs of probabilities
-    logits = model.predict(test_img)
-    ## select index of highest logits
-    ## Mars Start....
-    # scaler - 0 Dimension tensor i.e ()
-    # vector - 1 Dimension tensor i.e (9, )
-    # matrix - 2 Dimension tensor i.e (1, 6)
-    # output of tf.nn.softmax() is tf.EagerTensor
+    logits = model.predict(test_img)   
     proba = tf.nn.softmax(logits) 
-    # threshold = 0.9
-    ## numpy() - converts tf.EagerTensor into np.array
-    ## np.any() - any of the ele is true, return true
+    # --MOVE OUTSIDE OF THREAD--
+    # threshold = 0.9 
+    ## --any of the ele is true, return true--
     # if (proba > threshold).numpy().any():  
     #     predict_output = tf.argmax(logits, -1).numpy()
     # else:
     #     predict_output = [6]
-    ## Mars End....
-    ## run thru classifier to get name
-    # pred_text = class_names[predict_output[0]]   # running every frame...
-    ##---------------------------##
-    ## resize image
-    # img = tf.keras.utils.load_img(img, target_size=(299,299))
-    ## normalize
-    # img = tf.keras.utils.img_to_array(load_img).astype('float32')/255
-    # y_pred = model.predict(test_img, verbose=1)[0]
-    # y_pred_class = np.argmax(y_pred)
-    # y_pred_prob = y_pred[y_pred_class]*100
     pred_text=""
-    return pred_text, proba   # extract proba   
+    return pred_text, proba
     
 
-st.title("My first streamlit app")
-st.write("Hello, world-11")
+    
 
+st.title("Prettyfish classifier")
+st.write("Version 12")
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10,40)
@@ -88,50 +97,73 @@ fontScale = 1
 fontColor = (255,255,255)
 lineType = 2
 
-# note: the callback will be called in another thread
+
+
+# NOTE: the callback will be called in another thread
 # so use a queue here for thread-safety to pass the data
 # from inside to outside the callback
-result_queue: "queue.Queue[]" = queue.Queue()
+
+## init queue, queue.put(value), queue.get(value)
+result_queue: "queue.Queue[]" = queue.Queue()  
+# fish_class_queue: "queue.Queue[]" = queue.Queue() # not working, can't put 2 queues..
+fish = ""
+
+
+# -------------- Basic -----------------
+# def videoFilter(frame: av.VideoFrame) -> av.VideoFrame:
+#     img = frame.to_ndarray(format="bgr24")
+#     # --detect model here--
+#     pred_text, logits = process_img(img)
+#     text =  "HELLO! " + pred_text    
+#     cv2.putText(img, 
+#                 text,
+#                 bottomLeftCornerOfText,
+#                 font, 
+#                 fontScale, 
+#                 fontColor, 
+#                 lineType)
+#     result_queue.put(logits)
+#     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
 def videoFilter(frame: av.VideoFrame) -> av.VideoFrame:
     img = frame.to_ndarray(format="bgr24")
-    # detect model here!
+    # --detect model here--
     pred_text, proba = process_img(img)
-    text =  "HELLO! "+ pred_text     # text to print 
-    # cv2.putText(img, 
-    #             text,
-    #             bottomLeftCornerOfText,
-    #             font, 
-    #             fontScale, 
-    #             fontColor, 
-    #             lineType)
+    # fish_class = fish_class_queue.get() 
+    text =  "HELLO! " + fish
+    # --text to print--   
+    cv2.putText(img, 
+                text,
+                bottomLeftCornerOfText,
+                font, 
+                fontScale, 
+                fontColor, 
+                lineType)
+    # fish_class_queue.task_done()
     result_queue.put(proba)
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
 
 # webrtc_streamer(key="example")
-
 # webrtc_streamer(key="example", video_frame_callback=videoFilter)
 
 
-# to deploy it to cloud
-# config necessary to establish media streaming connection 
-# when the server is on remote host
-# streamlit_webrtc uses webRTC for its video and audio streaming
-# it has to access a STUN server in the global network for
-# the remote peers to establish WebRTC connections...
-# this code uses a free STUN server provided by Google
-# the value of the rtc_configuration argument will be passed to
-# the RTCPeerConnection constructor on the frontend.
-
-
+## NOTE: To deploy it to cloud
+## config necessary to establish media streaming connection 
+## when the server is on remote host
+## streamlit_webrtc uses webRTC for its video and audio streaming
+## it has to access a STUN server in the global network for
+## the remote peers to establish WebRTC connections...
+## this code uses a free STUN server provided by Google
+## the value of the rtc_configuration argument will be passed to
+## the RTCPeerConnection constructor on the frontend.
 webrtc_ctx = webrtc_streamer (
-    key="wtf666",
+    key="prettyfish",
     mode=WebRtcMode.SENDRECV,
     video_frame_callback = videoFilter,
-    rtc_configuration = { #Add this line
+    rtc_configuration = { # --Add this line--
         # "iceServers": [{"urls":["stun:stunserver.stunprotocol.org:3478",
         #                         "stun:stun2.l.google.com:19302",
         #                         "stun:stun.l.google.com:19302"]}]  
@@ -143,29 +175,48 @@ webrtc_ctx = webrtc_streamer (
 
 
 
+## NOTE: The video transformation with object detection and
+## this loop displaying the result labels are running
+## in different threads asynchronously.
+## Then the rendered video frames and the labels displayed here
+## are not strictly synchronized.
+
+# -------------- Basic -----------------
+# if st.checkbox("Show the detected labels", value=True):
+#     if webrtc_ctx.state.playing:
+#         labels_placeholder = st.empty()     
+#         while True:
+#             result = result_queue.get()
+#             labels_placeholder.table(result)
+#             result_queue.task_done()
+
+
+
+## -------------- Mars ---------------- ##
 if st.checkbox("Show logits", value=True):
     if webrtc_ctx.state.playing:
         labels_placeholder = st.empty()
-        # note: the video transformation with obj detection and
-        # this loop displaying the result labels are running
-        # in different threads asychronously
-        #then the rendered video frames and labels displayed here
-        # are not strictly synchronized
         probas = []
         threshold = 0.9
         while True:
             result = result_queue.get()
-            #st.write(result)
-            ## Mars start
             probas.append(tf.reshape(result, [6]))
-            if len(probas) > 10:  #keep last 5 frames...
+            # --keep last 5 frames--
+            if len(probas) > 10:     
                 probas = probas[1:]
-            avg = np.array(probas).mean(axis=0) # avg of last 5 probas
-            # labels_placeholder.table(avg)  # display probas
-            # # labels_placeholder.table(pd.DataFrame({"fish":[class_names[avg.argmax()]]}))  # display class
+            # --get average of last 5 probas--
+            avg = np.array(probas).mean(axis=0) 
+            # --display probas--
+            # labels_placeholder.table(avg)     
+            # --display class--
+            # labels_placeholder.table(pd.DataFrame({"fish":[class_names[avg.argmax()]]}))  
+            # --any avg of the ele is more than threshold, return true-- 
             if (avg>threshold).any():
                 fish_class = class_names[avg.argmax()]
             else:
                 fish_class = "not a fish"
-            
+            # fish_class_queue.put(fish_class) 
+            fish = fish_class
             labels_placeholder.table(pd.DataFrame({"fish":[fish_class]}))
+            result_queue.task_done()
+            
