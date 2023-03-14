652c77f7869847b23d5835e52e0866e822c25cc9
Arai Lanju <pearcearai@gmail.com>
2023-03-14 22:01:30 +0800

    ver 16

--- pages/app.py
+++ pages/app.py
@@ -89,7 +89,7 @@ def process_img (img):
     
 
 st.title("Prettyfish classifier")
-st.write("Version 14")
+st.write("Version 16")
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10,40)
