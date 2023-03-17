15b102fc9e621c056c795c0b180ee61a9237a1f8
Arai Lanju <pearcearai@gmail.com>
2023-03-17 13:42:37 +0800

    downgrade ver

--- pages/app.py
+++ pages/app.py
@@ -89,7 +89,7 @@ def process_img (img):
     
 
 st.title("Prettyfish classifier")
-st.write("Version 20")
+st.write("Version 21 - try using streamlit 1.18.0")
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10,40)
